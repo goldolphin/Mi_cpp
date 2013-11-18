085d3024a9041f09244c0295ccc66e13b1487b73
fuxicao <fuxicao@FUXICAO-DESKTOP.fareast.corp.microsoft.com>
2013-11-18 17:33:04 +0800

    ADD: code snippets

--- src/Main.cpp
+++ src/Main.cpp
@@ -11,24 +11,53 @@
 
 using namespace std;
 
-int fib(int n) {
-	if (n <= 1) {
-		return 1;
+class Frame {
+public:
+	void addVar(int value) {
+		variables.push_back(value);
 	}
-	return fib(n-1) + fib(n-2);
-}
+
+	int getVar(int id) {
+		return variables[id];
+	}
+
+private:
+	vector<int> variables;
+};
+
+class Environment {
+public:
+	void addFrame() {
+		frames.resize(frames.size()+1);
+	}
+
+	void removeFrame() {
+		frames.resize(frames.size()-1);
+	}
+
+	void addVar(int value, int frame = 0) {
+		frames[frames.size()-1-frame].addVar(value);
+	}
+
+	int getVar(int id, int frame = 0) {
+		return frames[frames.size()-1-frame].getVar(id);
+	}
+
+private:
+	vector<Frame> frames;
+};
 
 class IExpr {
 public:
 	virtual ~IExpr() {};
-	virtual int evaluate() = 0;
+	virtual int evaluate(Environment& env) = 0;
 };
 
 class Number : public IExpr {
 public:
 	Number(int value) : value(value) {}
 
-	virtual int evaluate() {
+	virtual int evaluate(Environment& env) {
 		return value;
 	}
 
@@ -36,16 +65,30 @@ private:
 	const int value;
 };
 
+class Variable : public IExpr {
+public:
+	Variable(int id, int frame) : id(id), frame(frame) {
+	}
+
+	virtual int evaluate(Environment& env) {
+		return env.getVar(id, frame);
+	}
+
+private:
+	int id;
+	int frame;
+};
+
 class If : public IExpr {
 public:
 	If (IExpr& predicate, IExpr& consequent, IExpr& alternative)
 		: predicate(predicate), consequent(consequent), alternative(alternative) {}
 
-	virtual int evaluate() {
-		if (predicate.evaluate() != 0) {
-			return consequent.evaluate();
+	virtual int evaluate(Environment& env) {
+		if (predicate.evaluate(env) != 0) {
+			return consequent.evaluate(env);
 		} else {
-			return alternative.evaluate();
+			return alternative.evaluate(env);
 		}
 	}
 
@@ -55,26 +98,113 @@ private:
 	IExpr& alternative;
 };
 
-class Environment {
-private:
-
+class IFunction {
+public:
+	virtual ~IFunction() {};
+	virtual int apply(Environment& env, vector<int>& params) = 0;
 };
 
-class Function {
+class Function : public IFunction {
 public:
-	Function(int paramNum, IExpr& body)
-		: paramNum(paramNum), body(body) {
+	Function(int paramNum)
+		: paramNum(paramNum), body(NULL) {
 	}
 
-	int apply(Environment env, vector<IExpr&> params) {
-		return 0;
+	void setBody(IExpr& body) {
+		this->body = &body;
+	}
+
+	virtual int apply(Environment& env, vector<int>& params) {
+		env.addFrame();
+		for (auto p = params.begin(); p != params.end(); p ++) {
+			env.addVar(*p);
+		}
+		int ret = body->evaluate(env);
+		env.removeFrame();
+		return ret;
 	}
 
 private:
 	int paramNum;
-	IExpr& body;
+	IExpr* body;
+};
+/*
+class FuncApply : public IExpr {
+public:
+	FuncApply(IFunction& function, IExpr& param1)
+		: function(function) {
+		params.push_back(param1);
+	}
+
+	FuncApply(IFunction& function, IExpr& param1, IExpr& param2)
+		: function(function) {
+		params.push_back(param1);
+		params.push_back(param2);
+	}
+
+	virtual int evaluate(Environment& env) {
+		vector<int> evaluated;
+		for (auto p = params.begin(); p != params.end(); p ++) {
+			evaluated.push_back((*p).evaluate(env));
+		}
+		return function.apply(env, evaluated);
+	}
+
+private:
+	IFunction& function;
+	vector<IExpr> params;
 };
 
+
+class LessEqual : public IFunction {
+public:
+	virtual int apply(Environment& env, vector<int>& params) {
+		return params[0] <= params[1];
+	}
+};
+
+class Add : public IFunction {
+public:
+	virtual int apply(Environment& env, vector<int>& params) {
+		return params[0] + params[1];
+	}
+};
+
+class Minus : public IFunction {
+public:
+	virtual int apply(Environment& env, vector<int>& params) {
+		return params[0] - params[1];
+	}
+};
+*/
+
+int fib(int n) {
+	if (n <= 1) {
+		return 1;
+	}
+	return fib(n-1) + fib(n-2);
+}
+
+/*
+int fib2(int n) {
+	LessEqual le;
+	Add add;
+	Minus minus;
+	Function fib(1);
+	Variable N(0, 0);
+	Number One(1);
+	Number Two(2);
+
+	FuncApply le1(le, N, One);
+	FuncApply minus1(minus, N, One);
+	FuncApply minus2(minus, N, Two);
+	FuncApply alternative(add, minus1, minus2);
+	If body(le1, One, alternative);
+	fib.setBody(body);
+	Environment env;
+	return (new FuncApply(fib, *(new Number(n))))->evaluate(env);
+}
+*/
 int main() {
 	for (int i = 0; i < 5; i ++) {
 		cout << fib(i) << endl;
